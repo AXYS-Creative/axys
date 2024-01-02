aaec61e03ee2ba5ad3d45aad07ac13b54c7978aa
axys-creative <axyscreativeagency@gmail.com>
2024-01-01 20:59:36 -0700

    marquee set up

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,16 @@
 import "./nav.js";
 import "./mouse-cursor.js";
 import "./animations.js";
+// import "./lenis.js"; // Screws with mouse div for some reason.
+
+// Clear focus from any element on mousemove (remove button :focus styles)
+(function clearFocusOnMouseMove() {
+  function removeFocus() {
+    if (document.activeElement) {
+      document.activeElement.blur();
+    }
+  }
+  // Set up event listeners
+  document.addEventListener("mousemove", removeFocus);
+  // window.addEventListener("scroll", removeFocus, true); // Buggy with tabbing to new sections
+})();
