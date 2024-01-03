2ebd5337626b1ad086354103fdec3de9ae399c2f
axys-creative <axyscreativeagency@gmail.com>
2024-01-03 07:55:16 -0700

    benefits sticky update

--- javascript/animations.js
+++ javascript/animations.js
@@ -94,3 +94,163 @@ const magnetEffect = () => {
 };
 
 magnetEffect();
+
+const gsapAnimations = () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Detailed markers for debugging
+
+  let whiteMarkers = {
+    startColor: "white",
+    endColor: "white",
+    indent: 128,
+  };
+
+  let navyMarkers = {
+    startColor: "navy",
+    endColor: "navy",
+    indent: 24,
+  };
+
+  let responsiveGsap = gsap.matchMedia();
+
+  responsiveGsap.add(
+    {
+      screenSm: "(max-width: 480px)",
+      screenMd: "(max-width: 768px)",
+      screenLg: "(min-width: 769px)",
+    },
+    (context) => {
+      let { screenSm, screenMd, screenLg } = context.conditions;
+
+      // Description - Pinning Benefits Headline
+      // gsap.to(".benefits-headline-full", {
+      //   scrollTrigger: {
+      //     trigger: ".benefits-headline-full",
+      //     start: screenLg ? "top center" : "top 16%",
+      //     // end: "+480%",
+      //     pin: true,
+      //     markers: whiteMarkers,
+      //   },
+      // });
+
+      // // Description - Shifting project images in Work Section
+      // function workItemTimeline(itemNumber, scrubFactor) {
+      //   return gsap
+      //     .timeline({
+      //       scrollTrigger: {
+      //         trigger: ".work",
+      //         scrub: scrubFactor,
+      //         start: "top 70%",
+      //         end: "+580%",
+      //         ease: "linear",
+      //       },
+      //     })
+      //     .to(`.work-item-${itemNumber}`, {
+      //       x: screenSm ? "-570vw" : screenMd ? "-288vw" : "-140vw",
+      //     });
+      // }
+
+      // const workItems = document.querySelectorAll(".work-item");
+
+      // workItems.forEach((el, index) => {
+      //   workItemTimeline(index + 1, 0.12 * (index + 1));
+      // });
+      // END - Shifting project images
+
+      // Descriptiion - Animate the membership prcing cards
+      // const animateMembershipCards = (selector, trigger, delay) => {
+      //   gsap.fromTo(
+      //     selector,
+      //     {
+      //       opacity: 0,
+      //       x: screenMd ? 120 : 200,
+      //     },
+      //     {
+      //       opacity: 1,
+      //       x: 0,
+      //       duration: 1.25,
+      //       delay: delay,
+      //       ease: screenMd ? "elastic.out(1, 0.75)" : "elastic.out(1, 0.4)",
+      //       scrollTrigger: {
+      //         trigger: trigger,
+      //         start: "top 80%",
+      //         end: screenMd ? "80% top" : "64% top",
+      //         toggleActions: "restart reverse restart reverse",
+      //       },
+      //     }
+      //   );
+      // };
+
+      // animateMembershipCards(".membership-card-1", ".membership-options", 0);
+      // animateMembershipCards(".membership-card-2", ".membership-options", 0.25);
+
+      // Description - Question List animation
+      // gsap.fromTo(
+      //   ".faq-item",
+      //   {
+      //     x: screenMd ? -24 : -48,
+      //     opacity: 0,
+      //     pointerEvents: "none",
+      //   },
+      //   {
+      //     x: 0,
+      //     opacity: 1,
+      //     pointerEvents: "all",
+      //     stagger: 0.08,
+      //     ease: screenMd ? "back.out(1)" : "back.out(2)",
+      //     scrollTrigger: {
+      //       trigger: ".questions-list",
+      //       start: "top 80%",
+      //       end: screenMd ? "80% top" : "64% top",
+      //       toggleActions: "restart reverse restart reverse",
+      //     },
+      //   }
+      // );
+    }
+  );
+
+  // Query only for large screen animations (Shift Work Title)
+  responsiveGsap.add("(min-width: 768px)", () => {
+    // Shift Title Text for Work Section. Large screens only***
+    const shiftWorkTitle = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".work",
+        scrub: true,
+        start: "top 10%",
+        end: "+620%",
+      },
+    });
+
+    shiftWorkTitle.fromTo(
+      ".word-selected",
+      {
+        translateX: "35%",
+      },
+      {
+        translateX: 0,
+      }
+    );
+
+    const shiftWorkTitle2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".work",
+        scrub: true,
+        start: "top 10%",
+        end: "+620%",
+      },
+    });
+
+    shiftWorkTitle2.fromTo(
+      ".word-work",
+      {
+        translateX: "60%",
+      },
+      {
+        translateX: 0,
+      }
+    );
+  });
+};
+
+gsapAnimations();
--- javascript/benefits.js
+++ javascript/benefits.js
@@ -11,6 +11,30 @@ const benefitData = [
     description:
       "Get designs or sections of your website generally within 2-3 business days (M-F). Please be aware that full websites can take weeks or even months to complete depending on their size and complexity.",
   },
+  {
+    title: "Seamless management",
+    icon: "../assets/graphics/user-check-light.svg",
+    description:
+      "Keeping track of progress is made easy with Trello. Simply sign in from your computer or phone to manage which tasks take priority over others. Watch as your requests are completed and delivered.",
+  },
+  {
+    title: "Fixed monthly rate",
+    icon: "../assets/graphics/tag-light.svg",
+    description:
+      "Don’t worry about inconsistent pricing that fluctuates throughout the year. Once signed up, your prices are locked in. If there are discounts offered you’re always eligible for those as well!",
+  },
+  {
+    title: "Unlimited team members",
+    icon: "../assets/graphics/user-plus-light.svg",
+    description:
+      "Need to add a team member to collaborate with? Just let us know and we can get them added to the Trello board. Then they’ll have access to submitting and tracking tickets.",
+  },
+  {
+    title: "Cancel or resume anytime",
+    icon: "../assets/graphics/play-pause-light.svg",
+    description:
+      "No contracts, ever. We understand that your workload can become light, so feel free to cancel. We also offer an easy to use “Pause” feature that freezes your account, preventing all charges and keeps your rate locked in.",
+  },
 ];
 
 const benefitList = document.querySelector(".benefit-list");
