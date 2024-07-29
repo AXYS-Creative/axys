0deab8186a62bc2b8dd8034e89267dcf65410c48
Aaron Garcia <axyscreativeagency@gmail.com>
2024-07-29 08:10:47 -0600

    cleaned up custom focus js

--- javascript/utility.js
+++ javascript/utility.js
@@ -1,17 +1,3 @@
-// // Clear focus from any element on mousemove (remove button :focus styles)
-// (function clearFocusOnMouseMove() {
-//   function removeFocus() {
-//     if (
-//       document.activeElement &&
-//       !document.activeElement.classList.contains("input")
-//     ) {
-//       document.activeElement.blur();
-//     }
-//   }
-//   // Set up event listeners
-//   document.addEventListener("mousemove", removeFocus);
-// })();
-
 // Get Current Year for Copyright
 (function getCurrentYear() {
   const yearText = document.querySelector(".year-text");
