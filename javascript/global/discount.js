ad48017ab02d281b26f65b7d24347e0961981963
Aaron Garcia <axyscreativeagency@gmail.com>
2024-12-27 17:23:07 -0700

    toast delay for loader

--- javascript/global/discount.js
+++ javascript/global/discount.js
@@ -12,8 +12,7 @@ const discountCountdownTimer = () => {
  * Using a timer so I don't need to manually update this at midnight on new years eve... Just the stripe links 🥲
  * This also accepts a boolean value if you'd like to change it manually.
  */
-// let discount = discountCountdownTimer();
-let discount = false;
+let discount = discountCountdownTimer();
 
 //
 // Plans Section
